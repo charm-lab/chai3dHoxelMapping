a99e8552f29ec6aaad921b9d8be2a1d6c9d8be54
jpalms1 <63813134+jpalms1@users.noreply.github.com>
2022-11-02 13:39:12 -0700

    11-02-2022
    Finish implementing Dunett multi-comparison test for HME self-pilot data.
    
    Made plots appear in 3x3 grid using local version of meansgraph.m from the Stats and Machine Learning ToolBox.
    
    Will make version to conditionally switch between interactable and non-interactable version

--- MatLab Scripts/runHMEStats.m
+++ MatLab Scripts/runHMEStats.m
@@ -6,21 +6,25 @@ function runHMEStats()
     % 2-way ANOVA
     % p = anovan(y, group)
     showStats = "off";
-    format short
+    format long
+    close all;
+    fontSize = 10;
+    width = 1700;
+    height = 1000;
     % Groups:
     %Mapping group:
-    mappings(1:10,1) = {'1'};
+    mappings(1:10,1)  = {'1'};
     mappings(11:20,1) = {'2'};
     mappings(21:30,1) = {'3'};
     mappings(31:40,1) = {'4'};
     mappings(41:50,1) = {'5'};
     
     %Visibility condition
-    visibiltyCondition(1:10,1)   = {'visibleCube'};
-    visibiltyCondition(11:20,1) = {'visibleCube'};
-    visibiltyCondition(21:30,1) = {'visibleCube'};
-    visibiltyCondition(31:40,1) = {'visibleCube'};
-    visibiltyCondition(41:50,1) = {'visibleCube'};
+    %     visibiltyCondition(1:10,1)  = {'visibleCube'};
+    %     visibiltyCondition(11:20,1) = {'visibleCube'};
+    %     visibiltyCondition(21:30,1) = {'visibleCube'};
+    %     visibiltyCondition(31:40,1) = {'visibleCube'};
+    %     visibiltyCondition(41:50,1) = {'visibleCube'};
     
     %Convert matrix of subject results to column vectors
     %CompletionTime Mapping 1 visCube group
@@ -127,66 +131,79 @@ function runHMEStats()
     %Completion Time
     yCT = [yCT_visMap1; yCT_visMap2; yCT_visMap3; yCT_visMap4; yCT_visMap5];
     %     p_CompletionTime = anovan(yCT, {mappings})
-    [p_CompletionTime, tbl_CompletionTime , stats_CompletionTime ] = ...
+    [p_CompletionTime, ~, stats_CompletionTime ] = ...
         anovan(yCT,  {mappings},...
         "Model","interaction", "Varnames",["mappings"],...
         "display",showStats);
     figure(1)
-    multcompare(stats_CompletionTime, "CriticalValueType", "dunnett", ...
+    subplot(3,3,1)
+    compCT = multcompare(stats_CompletionTime, "CriticalValueType", "dunnett", ...
         "ControlGroup", 5, "Alpha", 0.05);
     %Interactable comparison with any group:
     %     multcompare(stats_CompletionTime);
     %     title("Completion Time -- Interactable")
     title("Completion Time")
-    improvePlot_v2(false, true, 10, 500, 500)
+    improvePlot_v2(false, true, fontSize, width, height)
+
+    tbl_CompletionTime = array2table(compCT,"VariableNames", ...
+    ["Group A","Group B","Lower Limit","A-B","Upper Limit","P-value"])
 
     %Index Path Length
     yIPL = [yIPL_visMap1; yIPL_visMap2; yIPL_visMap3; yIPL_visMap4; yIPL_visMap5];
     %     p_IndexPathLength = anovan(yIPL, {mappings})
-    [p_IndexPathLength, tbl_IndexPathLength, stats_IndexPathLength] = ...
+    [p_IndexPathLength, ~, stats_IndexPathLength] = ...
         anovan(yIPL,  {mappings},...
         "Model","interaction", "Varnames",["mappings"],...
         "display",showStats);   
-    figure(2)
-    multcompare(stats_IndexPathLength, "CriticalValueType", "dunnett", ...
+    subplot(3,3,2)
+    compIPL = multcompare(stats_IndexPathLength, "CriticalValueType", "dunnett", ...
         "ControlGroup", 5, "Alpha", 0.05);
     %Interactable comparison with any group:
     %     multcompare(stats_IndexPathLength);
     %     title("Index Path Length -- Interactable")
     title("Index Path Length")
-    improvePlot_v2(false, true, 10, 500, 500)
+    improvePlot_v2(false, true, fontSize, width, height)
+
+    tbl_IndexPathLength = array2table(compIPL,"VariableNames", ...
+    ["Group A","Group B","Lower Limit","A-B","Upper Limit","P-value"])
 
     %Thumb Path Length
     yTPL = [yTPL_visMap1; yTPL_visMap2; yTPL_visMap3; yTPL_visMap4; yTPL_visMap5];
     %     p_ThumbPathLength = anovan(yTPL, {mappings})
-    [p_ThumbPathLength, tbl_ThumbPathLength, stats_ThumbPathLength] = ...
+    [p_ThumbPathLength, ~, stats_ThumbPathLength] = ...
         anovan(yTPL,  {mappings},...
         "Model","interaction", "Varnames",["mappings"],...
         "display",showStats);    
-    figure(3)
-    multcompare(stats_ThumbPathLength, "CriticalValueType", "dunnett", ...
+    subplot(3,3,3)
+    compTPL = multcompare(stats_ThumbPathLength, "CriticalValueType", "dunnett", ...
         "ControlGroup", 5, "Alpha", 0.05);
     %Interactable comparison with any group:
     %     multcompare(stats_ThumbPathLength);
     %     title("Thumb Path Length -- Interactable")
     title("Thumb Path Length")
-    improvePlot_v2(false, true, 10, 500, 500)
+    improvePlot_v2(false, true, fontSize, width, height)
+
+    tbl_ThumbPathLength = array2table(compTPL,"VariableNames", ...
+    ["Group A","Group B","Lower Limit","A-B","Upper Limit","P-value"])
 
     %Box Path Length
     yBPL = [yBPL_visMap1; yBPL_visMap2; yBPL_visMap3; yBPL_visMap4; yBPL_visMap5];
     %     p_BoxPathLength = anovan(yBPL, {mappings})
-    [p_BoxPathLength, tbl_BoxPathLength, stats_BoxPathLength] = ...
+    [p_BoxPathLength, ~, stats_BoxPathLength] = ...
         anovan(yBPL,  {mappings},...
         "Model","interaction", "Varnames",["mappings"],...
         "display",showStats);
-    figure(4)
-    multcompare(stats_BoxPathLength, "CriticalValueType", "dunnett", ...
+    subplot(3,3,4)
+    compBPL = multcompare(stats_BoxPathLength, "CriticalValueType", "dunnett", ...
         "ControlGroup", 5, "Alpha", 0.05);
     %Interactable comparison with any group:
     %     multcompare(stats_BoxPathLength);
     %     title("Box Path Length -- Interactable")
     title("Box Path Length")
-    improvePlot_v2(false, true, 10, 500, 500)
+    improvePlot_v2(false, true, fontSize, width, height)
+
+    tbl_BoxPathLength = array2table(compBPL,"VariableNames", ...
+    ["Group A","Group B","Lower Limit","A-B","Upper Limit","P-value"])
 
     %Index Shear Force
     yISF = [yISF_visMap1; yISF_visMap2; yISF_visMap3; yISF_visMap4; yISF_visMap5];
@@ -195,89 +212,101 @@ function runHMEStats()
         anovan(yISF,  {mappings},...
         "Model","interaction", "Varnames",["mappings"],...
         "display",showStats);    
-    figure(5)
-    multcompare(stats_IndexShearForce, "CriticalValueType", "dunnett", ...
+    subplot(3,3,5)
+    compISF = multcompare(stats_IndexShearForce, "CriticalValueType", "dunnett", ...
         "ControlGroup", 5, "Alpha", 0.05);
     %Interactable comparison with any group:
     %     multcompare(stats_IndexShearForce);
     %     title("Index Shear Force -- Interactable")
     title("Index Shear Force")
-    improvePlot_v2(false, true, 10, 500, 500)
+    improvePlot_v2(false, true, fontSize, width, height)
+
+    tbl_IndexShearForce = array2table(compISF,"VariableNames", ...
+    ["Group A","Group B","Lower Limit","A-B","Upper Limit","P-value"])
 
     %Index Normal Force
     yINF = [yINF_visMap1; yINF_visMap2; yINF_visMap3; yINF_visMap4; yINF_visMap5];
     %     p_IndexNormalForce = anovan(yINF, {mappings})
-    [p_IndexNormalForce, tbl_IndexNormalForce, stats_IndexNormalForce] = ...
+    [p_IndexNormalForce, ~, stats_IndexNormalForce] = ...
         anovan(yINF,  {mappings},...
-        "Model","interaction", "Varnames",["mappings"],...
+        "Model","interaction", "Varnames", ["mappings"],...
         "display",showStats);
-    figure(6)
-    multcompare(stats_IndexNormalForce, "CriticalValueType","dunnett", ...
+    subplot(3,3,6)
+    compINF = multcompare(stats_IndexNormalForce, "CriticalValueType","dunnett", ...
         "ControlGroup", 5, "Alpha", 0.05);
     %Interactable comparison with any group:
     %     multcompare(stats_IndexNormalForce);
     %     title("Index Normal Force -- Interactable")
     title("Index Normal Force")
-    improvePlot_v2(false, true, 10, 500, 500)
+    improvePlot_v2(false, true, fontSize, width, height)
+
+    tbl_IndexNormalForce = array2table(compINF,"VariableNames", ...
+    ["Group A","Group B","Lower Limit","A-B","Upper Limit","P-value"])
 
     %Thumb Shear Force
     yTSF = [yTSF_visMap1; yTSF_visMap2; yTSF_visMap3; yTSF_visMap4; yTSF_visMap5];
     %     p_ThumbShearForce = anovan(yTSF, {mappings})
-    [p_ThumbShearForce, tbl_ThumbShearForce, stats_ThumbShearForce] = ...
+    [p_ThumbShearForce, ~, stats_ThumbShearForce] = ...
         anovan(yTSF,  {mappings},...
-        "Model","interaction", "Varnames",["mappings"],...
+        "Model","interaction", "Varnames", ["mappings"],...
         "display",showStats);
-    figure(7)
-    %subplot(1,3,1)
+    
     %Compare signifcance with control group only:
-    multcompare(stats_ThumbShearForce, "CriticalValueType", "dunnett", ...
+    subplot(3,3,7)
+    compTSF = multcompare(stats_ThumbShearForce, "CriticalValueType", "dunnett", ...
         "ControlGroup", 5, "Alpha", 0.05);
     %Interactable comparison with any group:
     %     multcompare(stats_ThumbShearForce);
     %     title("Thumb Shear Force -- Interactable")
     title("Thumb Shear Force")
-    improvePlot_v2(false, true, 10, 500, 500) 
+    improvePlot_v2(false, true, fontSize, width, height)       
+
+    tbl_ThumbShearForce = array2table(compTSF,"VariableNames", ...
+    ["Group A","Group B","Lower Limit","A-B","Upper Limit","P-value"])
 
     %Thumb Normal Force
     yTNF = [yTNF_visMap1; yTNF_visMap2; yTNF_visMap3; yTNF_visMap4; yTNF_visMap5];
     %     p_ThumbNormalForce = anovan(yTNF, {mappings})
-    [p_ThumbNormalForce, tbl_ThumbNormalForce, stats_ThumbNormalForce] = ...
+    [p_ThumbNormalForce, ~, stats_ThumbNormalForce] = ...
         anovan(yTNF,  {mappings},...
-        "Model","interaction", "Varnames",["mappings"],...
+        "Model","interaction", "Varnames", ["mappings"],...
         "display",showStats); 
-    figure(8)
+
     %Compare signifcance with control group only:
-    multcompare(stats_ThumbNormalForce, "CriticalValueType", "dunnett", ...         
+    subplot(3,3,8) 
+    compTNF = multcompare(stats_ThumbNormalForce, "CriticalValueType", "dunnett", ...         
         "ControlGroup", 5, "Alpha", 0.05); 
     %Interactable comparison with any group:
     %   multcompare(stats_ThumbNormalForce); 
     %   title("Thumb Normal Force -- Interactable") 
-    improvePlot_v2(false, true, 10, 500, 500) 
+    improvePlot_v2(false, true, fontSize, width, height) 
     title("Thumb Normal Force -- Control")
-    %subplot(1,3,2)  
+    
+
+    tbl_ThumbNormalForce = array2table(compTNF,"VariableNames", ...
+    ["Group A","Group B","Lower Limit","A-B","Upper Limit","P-value"])
 
     %Box Drops
+    subplot(3,3,9)
     yBD = [yBD_visMap1; yBD_visMap2; yBD_visMap3; yBD_visMap4; yBD_visMap5];
     %     p_BoxDrops = anovan(yBD, {mappings})
-    [p_BoxDrops, ~ , stats_BoxDrops] = ...
-        anovan(yCT,  {mappings},...
-        "Model","interaction", "Varnames",["mappings"],...
+    [p_BoxDrops, ~, stats_BoxDrops] = ...
+        anovan(yBD,  {mappings},...
+        "Model","interaction", "Varnames", ["mappings"],...
         "display",showStats);
-    figure(9)
     %Compare signifcance with control group only:
     compBD = multcompare(stats_BoxDrops, "CriticalValueType", "dunnett", ...
         "ControlGroup", 5, "Alpha", 0.05); 
     %Interactable comparison with any group:
     %   multcompare(stats_BoxDrops); 
     %   title("BoxDrops -- Interactable")    
-    improvePlot_v2(false, true, 10, 500, 500)
+    improvePlot_v2(false, true, fontSize, width, height)
     title("BoxDrops -- Control")
-    %subplot(1,3,3)
-    
+
     tbl_BoxDrops = array2table(compBD,"VariableNames", ...
     ["Group A","Group B","Lower Limit","A-B","Upper Limit","P-value"])
-
-    if (showStats == "off")
+    
+    if (showStats == "on")
         p_CompletionTime
         p_IndexPathLength
         p_ThumbPathLength
@@ -287,6 +316,5 @@ function runHMEStats()
         p_ThumbShearForce
         p_ThumbNormalForce
         p_BoxDrops
-    end
-        
+    end       
 end
